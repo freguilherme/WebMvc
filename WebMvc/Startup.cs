ba71a7cc3f023c704a1853642eea764e693058c7
Guilherme Fre <guiaafre@gmail.com>
2021-11-09 21:38:00 -0300

    crud scaffolding

--- WebMvc/Startup.cs
+++ WebMvc/Startup.cs
@@ -1,4 +1,4 @@
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
@@ -8,6 +8,8 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using WebMvc.Data;
 
 namespace WebMvc
 {
@@ -24,6 +26,9 @@ namespace WebMvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+
+            services.AddDbContext<WebMvcContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("WebMvcContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
