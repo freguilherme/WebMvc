d2471bf95d9c7ac86a19238097cb4ef050907be8
Guilherme Fre <guiaafre@gmail.com>
2021-11-10 21:21:11 -0300

    tela simples de cadastro

--- WebMvc/Controllers/SellersController.cs
+++ WebMvc/Controllers/SellersController.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WebMvc.Models;
 using WebMvc.Services;
 
 namespace WebMvc.Controllers
@@ -20,6 +21,20 @@ namespace WebMvc.Controllers
         {
             var list = _sellerService.FindAll();
             return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerService.Insert(seller);
+
+            return RedirectToAction(nameof(Index));
         }
     }
 }
