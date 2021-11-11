b077e129e6b6bd238568e0d0d2478d3c74bc411b
Guilherme Fre <guiaafre@gmail.com>
2021-11-10 23:43:45 -0300

    tela de detalhes

--- WebMvc/Services/SellerService.cs
+++ WebMvc/Services/SellerService.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using WebMvc.Data;
 using WebMvc.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebMvc.Services
 {
@@ -27,7 +28,7 @@ namespace WebMvc.Services
 
         public Seller FindById(int id)
         {
-            return _context.Seller.FirstOrDefault(obj => obj.Id == id);
+            return _context.Seller.Include(obj => obj.Department).FirstOrDefault(obj => obj.Id == id);
         }
 
         public void Remove(int id)
