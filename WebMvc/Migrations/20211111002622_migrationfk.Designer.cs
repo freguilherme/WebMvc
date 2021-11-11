9c022d133fb8271e716a12cc67170e53971783e1
Guilherme Fre <guiaafre@gmail.com>
2021-11-10 21:29:57 -0300

    fk não nula, integridade referencial

--- WebMvc/Migrations/20211111002622_migrationfk.Designer.cs
+++ WebMvc/Migrations/20211111002622_migrationfk.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WebMvc.Data;
 
 namespace WebMvc.Migrations
 {
     [DbContext(typeof(WebMvcContext))]
-    partial class WebMvcContextModelSnapshot : ModelSnapshot
+    [Migration("20211111002622_migrationfk")]
+    partial class migrationfk
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
