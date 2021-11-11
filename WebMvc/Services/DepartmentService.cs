728e781e0752e627de37e67c5eae99d078e8a2ca
Guilherme Fre <guiaafre@gmail.com>
2021-11-10 21:51:39 -0300

    select de departamentos

---
+++ WebMvc/Services/DepartmentService.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using WebMvc.Data;
+using WebMvc.Models;
+
+namespace WebMvc.Services
+{
+    public class DepartmentService
+    {
+        private readonly WebMvcContext _context;
+
+        public DepartmentService(WebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Department> FindAll()
+        {
+            return _context.Department.OrderBy(x => x.Name).ToList();
+        }
+    }
+}
--- WebMvc/Services/SellerService.cs
+++ WebMvc/Services/SellerService.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 using WebMvc.Data;
 using WebMvc.Models;
 
@@ -23,7 +21,6 @@ namespace WebMvc.Services
 
         public void Insert(Seller obj)
         {
-            obj.Department = _context.Department.First();
             _context.Add(obj);
             _context.SaveChanges();
         }
