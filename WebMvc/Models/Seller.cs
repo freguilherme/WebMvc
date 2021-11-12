9373e8ea608bf4e320c8f936b864868a0efbb785
Guilherme Fre <guiaafre@gmail.com>
2021-11-11 22:16:03 -0300

    validação

--- WebMvc/Models/Seller.cs
+++ WebMvc/Models/Seller.cs
@@ -10,19 +10,25 @@ namespace WebMvc.Models
         public int Id { get; set; }
 
         [Display(Name = "Nome")]
+        [Required(ErrorMessage = "Nome obrigatorio")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} deve conter de {2} a {1} caracteres")]
         public string Name { get; set; }
 
         [Display(Name = "E-mail")]
         [DataType(DataType.EmailAddress)]
+        [Required]
         public string email { get; set; }
 
         [Display(Name = "Data Nascimento")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}")]
+        [Required]
         public DateTime BirthDate { get; set; }
 
         [Display(Name = "Salário Base")]
         [DisplayFormat(DataFormatString = "{0:F2}")]
+        [Required]
+        [Range(100.00, 15000.00, ErrorMessage = "Salário deve ser entre {1} e {2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
