41e46698b5317b18fe086207b0438f1bcdf3953e
Guilherme Fre <guiaafre@gmail.com>
2021-11-15 13:49:06 -0300

    exceção delete

--- WebMvc/Services/SellerService.cs
+++ WebMvc/Services/SellerService.cs
@@ -35,9 +35,16 @@ namespace WebMvc.Services
 
         public async Task RemoveAsync(int id)
         {
-            var obj = await _context.Seller.FindAsync(id);
-            _context.Seller.Remove(obj);
-            await _context.SaveChangesAsync();
+            try
+            {
+                var obj = await _context.Seller.FindAsync(id);
+                _context.Seller.Remove(obj);
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateException e)
+            {
+                throw new IntegrityException("Impossivel deletar o vendedor, ela/ele tem vendas");
+            }
         }
 
         public async Task UpdateAsync(Seller obj)
